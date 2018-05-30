5665537fc2387d510c8ecc936cbfdd54326577cb
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2018-05-30 13:33:55 +0200

    Register GSE strings in Polylang

--- includes/class-genesis-simple-edits-compat.php
+++ includes/class-genesis-simple-edits-compat.php
@@ -10,6 +10,19 @@ class Genesis_Simple_Edits_Compat {
 	private $settings_field;
 
 	/**
+	 * The available fields.
+	 *
+	 * @since 2.2.2
+	 */
+	private $fields = array(
+		'post_info' => '',
+		'post_meta' => '',
+		'footer_backtotop_text' => '',
+		'footer_creds_text' => '',
+		'footer_output' => '',
+	);
+
+	/**
 	 * Constructor.
 	 */
 	public function __construct() {
@@ -19,6 +32,17 @@ class Genesis_Simple_Edits_Compat {
 	}
 
 	public function init() {
+
+		if ( function_exists( 'pll_register_string' ) ) {
+
+			foreach ( $this->fields as $field => $string ) {
+				// Get strings. Do not use cache!
+				$string = genesis_get_option( $field, $this->settings_field, false );
+				// Register string in Polylang.
+				pll_register_string( 'post_info', $string, 'genesis-simple-edits', true );
+			}
+
+		}
 
 	}
 
